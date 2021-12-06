842dff1d38efd2e2b6089088325f21f7b7d589fd
yesora <valsa_de_euridice@naver.com>
2021-12-06 11:20:57 +0900

    file download 이후 엣지로 요청정보 전송

--- src/main/java/com/penta/edgesupporter/controller/FileDownload.java
+++ src/main/java/com/penta/edgesupporter/controller/FileDownload.java
@@ -13,6 +13,12 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,13 +33,57 @@ public class FileDownload {
     @SneakyThrows
     public ResponseEntity<?> giveDataFileToCentral(@PathVariable String filehash, HttpServletRequest req) {
 
+        LocalDateTime time = LocalDateTime.now();
         log.info("file요청" + req.getRequestURI());
         log.info("----giveDataFileToCentral----");
 
+        /*파일명으로 파일 찾기*/
+        File f = new File(fileManager.getVehicleLocation().toString()+"/"+filehash+".csv.gz");
+
+        if(f.exists()) {
+
+            /*
+             프록시 환경 또는 클라우드 위에 있는 웹 응용 프로그램의 경우 HTTP 요청 헤더 X-Forwarded-For(XFF)를 통해 클라이언트 IP 주소를 가져와야 한다.
+            WAS 는 보통 2차 방화벽 안에 있고 Web Server 를 통해 client 에서 호출되거나 cluster로 구성되어 load balancer 에서 호출된다.
+            이럴 경우에서 getRemoteAddr() 을 호출하면 접근한 클라이언트의 외부아이피가 아닌 웹서버나 load balancer의 IP 가 나오면서 같은아이피가 계속 찍히게 된다.
+            위와 같은 문제를 해결하기 위해 사용되는 HTTP Header인 X-Forwarded-For 값을 확인해서 있으면 해당 키값을 사용하고 없으면 getRemoteAddr() 를 사용한다.
+            * */
+            String clientIP = (null != req.getHeader("X-FORWARDED-FOR")) ? req.getHeader("X-FORWARDED-FOR") : req.getRemoteAddr();
+            Resource resource = new FileSystemResource(f);
+            long fileLength = f.length();
+
+            /*
+            * tracing history를 남기기 위해 edge로 소켓 통신
+            * format
+            * {[{sptoedge9812::DATA_ID::RECEIVED_TIME::CLIENT_IP::}]}
+            */
+
+            try (Socket socket = new Socket("127.0.0.1", 17300);
+                 OutputStream output = socket.getOutputStream();) {
+                //{[{REQ::ID::REQ_CODE::REQ_DATA}]}
+                String reqString = "{[{sptoedge9812::" + filehash + "::"+time.toString()+"::" + clientIP + "::}]}";
+                byte[] data = reqString.getBytes(StandardCharsets.US_ASCII);
+                output.write(data);
+                output.flush();
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + resource.getFilename() + "\"")
+                    .contentLength(fileLength)
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .body(resource);
+        }
+
+
+        /* 파일 해시로 파일 찾기 */
+        /*
+
         File dir = new File(fileManager.getVehicleLocation().toString());
         File[] fileList = dir.listFiles();
-
-        long fileLength = 0L;
 
         for(File file : fileList) {
             if(filehash.equals(fileManager.getHash(file))) {
@@ -48,6 +98,7 @@ public class FileDownload {
                         .body(resource);
             }
         }
+         */
 
         return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
     }
