4a7b034a8101e1722553aee3bab7bc13601199f5
YunKeon112 <kyun@pentasecurity.com>
2021-12-31 02:55:42 +0900

    add sendpacket

--- test/src/default.js
+++ test/src/default.js
@@ -4,4 +4,9 @@ $(document).ready(function(){
 	$(".action_register").click(function(){
 		$.register();
 	});
+
+    $(".action_send_packet").click(function(){
+		var idx = $(this).attr("idx");
+		$.send_packet(idx);
+	});
 });
