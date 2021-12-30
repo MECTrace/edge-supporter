90b5b9cb5d2c5b888728bf3d55274868d9dd8b8f
YunKeon112 <kyun@pentasecurity.com>
2021-12-31 03:19:48 +0900

    Clear channel 2 interrupt flag

--- test/src/main.c
+++ test/src/main.c
@@ -10,4 +10,5 @@ void delay(volatile int cycles)
 void PIT_Ch2_IRQHandler(void)
 {
     PINS_DRV_TogglePins(LED7_PORT, LED7);       
+    PIT_DRV_ClearStatusFlags(INST_PIT1, pit1_ChnConfig0.hwChannel);  
 }
