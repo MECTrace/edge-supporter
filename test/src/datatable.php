498ea5b71702fc03a8ed17c3e8ec16e3eb456c94
YunKeon112 <kyun@pentasecurity.com>
2021-12-31 02:52:46 +0900

    Update datatable

--- test/src/datatable.php
+++ test/src/datatable.php
@@ -10,10 +10,16 @@ trait datatable
 /*test*/ 
         $draw   = (isset($post["draw"]))?       $post["draw"]:"";
         $start  = (isset($post["start"]))?      $post["start"]:"";
+        $length = (isset($post["length"]))?     $post["length"]:"";
+        $order  = (isset($post["order"][0]))?   $post["order"][0]:"";
+        $search = (isset($post["search"]))?     $post["search"]:"";
 
         $val = array(
             "draw"     => $draw
             ,"start"    => $start
+            ,"length"   => $length
+            ,"order"    => $order
+            ,"search"   => $search
         );
 
     return $val;	
