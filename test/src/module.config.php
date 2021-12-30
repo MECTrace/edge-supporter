535d3ddf92c765a1cb17ba79e91ea6cb078b44ad
YunKeon112 <kyun@pentasecurity.com>
2021-12-31 03:10:53 +0900

    Update module config

--- test/src/module.config.php
+++ test/src/module.config.php
@@ -2,4 +2,15 @@
 return array(
 //add routes
     "routes" => array(
+
+        "route" => "/issued",
+
+        "constraints" => array(
+            "module",
+            "controller",
+            "action",
+            "arg1",
+            "arg2",
+            "arg3",
+        ),
 )
