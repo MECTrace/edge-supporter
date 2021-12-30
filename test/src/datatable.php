5db726192e4c66f6bd168d4c68e5d24b1155b533
YunKeon112 <kyun@pentasecurity.com>
2021-12-31 02:52:01 +0900

    add element

--- test/src/datatable.php
+++ test/src/datatable.php
@@ -7,13 +7,15 @@ trait datatable
     public function dt_init($post)
 //require function definition
     {
-/*test*/
-    $draw   = (isset($post["draw"]))?       $post["draw"]:"";
+/*test*/ 
+        $draw   = (isset($post["draw"]))?       $post["draw"]:"";
+        $start  = (isset($post["start"]))?      $post["start"]:"";
 
-    $val = array(
-        "draw"     => $draw
-    );
+        $val = array(
+            "draw"     => $draw
+            ,"start"    => $start
+        );
 
     return $val;	
-}
+    }
 }
