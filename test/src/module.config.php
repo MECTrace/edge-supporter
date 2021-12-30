b9493f20d6fa8002c414ff7556885a09c94987b4
YunKeon112 <kyun@pentasecurity.com>
2021-12-31 03:11:15 +0900

    update module config

--- test/src/module.config.php
+++ test/src/module.config.php
@@ -18,4 +18,10 @@ return array(
             "controller"    => "index",
             "action"        => "index",
         ),
+
+        "view_manager" => array(
+            //"layout"    	=> __DIR__ . DS . "view" . DS . "layout" . DS . "layout.phtml",
+            "html_path"   	=> __DIR__ . DS . "view" . DS . "html",
+            //"error"     => __DIR__ . DS . "view" . DS . "error" . DS . "error.phtml",
+        ),
 )
