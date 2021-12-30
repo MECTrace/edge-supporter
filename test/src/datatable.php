9ff4be59920222a6a496425a0bb4d670a43b58a1
YunKeon112 <kyun@pentasecurity.com>
2021-12-31 02:50:35 +0900

    Update datatable

--- test/src/datatable.php
+++ test/src/datatable.php
@@ -6,4 +6,11 @@ trait datatable
 {
     public function dt_init($post)
 //require function definition
+
+/*test*/
+    $draw   = (isset($post["draw"]))?       $post["draw"]:"";
+
+    $val = array(
+        "draw"     => $draw
+    );
 }
